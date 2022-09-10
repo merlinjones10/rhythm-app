194c32e9d46830b3db8182b315d44c02c0d540ed
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2022-09-10 01:28:22 +0100

    working bar of random rhythms

--- RhythmRandom/ContentView.swift
+++ RhythmRandom/ContentView.swift
@@ -3,20 +3,44 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var viewModel : RhythmRandomizerVM
     
+    let data = Array(1...4).map{"item\($0)"}
+    
+    let oneRowGrid = [GridItem(.fixed(4))]
+    
     var body: some View {
-        HStack() {
-            Spacer()
-            Image(systemName: "arrow.left").border(.green).font(.largeTitle).onTapGesture {
-                viewModel.randomize()
+        VStack{
+            Text("Notes").font(.title2)
+            LazyHGrid(rows: oneRowGrid, spacing: 10) {
+                ForEach(viewModel.bar, id: \.self) {item in
+                    Image(item.name)
+                }
             }
-            Spacer()
-            NoteView(note: viewModel.onlyRiddim)
-            Spacer()
-            Image(systemName: "arrow.right").border(.green).font(.largeTitle).onTapGesture {
-                viewModel.randomize()
-            }
-            Spacer()
+            HStack {
+                Spacer()
+                Image(systemName: "arrow.left")
+                Spacer()
+                Image(systemName: "dice").onTapGesture {
+                    viewModel.randomizeRhythms()
+                }
+                Spacer()
+                Image(systemName: "arrow.right")
+                Spacer()
+            }.padding()
         }
+
+//        HStack() {
+//            Spacer()
+//            Image(systemName: "arrow.left").border(.green).font(.largeTitle).onTapGesture {
+//                viewModel.randomize()
+//            }
+//            Spacer()
+//            NoteView(note: viewModel.onlyRiddim)
+//            Spacer()
+//            Image(systemName: "arrow.right").border(.green).font(.largeTitle).onTapGesture {
+//                viewModel.randomize()
+//            }
+//            Spacer()
+//        }
     }
 }
 
---
+++ functionFun.playground/Contents.swift
@@ -0,0 +1,76 @@
+import Cocoa
+
+struct Rhythm {
+var name: String
+var value: Double
+var rest: Bool = false
+}
+
+let base = 1
+let minim = Rhythm(name: "minim", value: 2.0)
+let crotchet = Rhythm(name: "crotchet", value: 1.0)
+let dottedCrotchet = Rhythm(name: "dottedCrotchet", value: 1.5)
+let quaver = Rhythm(name: "quaver", value: 0.5)
+let quaverPair = Rhythm(name: "quaverPair", value: 1.0)
+let semiQuaver = Rhythm(name: "semiQuaver", value: 0.25)
+
+var barLength = 4.0
+let availableNotes: Array<Rhythm> = [crotchet, quaver, quaverPair, semiQuaver, dottedCrotchet, minim]
+
+
+func createBar(choices: Array<Rhythm>, length: Double) -> Array<Rhythm>  {
+    // Error handling.
+    var bar: Array<Rhythm> = []
+    var amountLeftInBar = length
+    var totalCurrentBarLength = 0.0
+    var availableChoices = choices
+    
+    while (totalCurrentBarLength < length) {
+        amountLeftInBar = length - totalCurrentBarLength
+        availableChoices = choices.filter { rhythm in
+            return rhythm.value <= amountLeftInBar
+        }
+        if let randomChoice = availableChoices.randomElement() {
+            bar.append(randomChoice)
+            totalCurrentBarLength += randomChoice.value
+        } else {
+            print("No choices left")
+            break
+        }
+    }
+    return bar
+}
+
+
+let myBar = createBar(choices: availableNotes, length: 4.0)
+
+var total = 0.0
+
+for val in myBar {
+    print(val.name)
+    total += val.value
+    
+}
+print("\nTOTAL: \(total)")
+
+
+
+
+//func createBar(choices: Array<Rhythm>, length: Double) {
+//    // <- choice of values
+//    // set a running total
+//    var totalCurrentBarLength = 0.0
+//    // init an empty array
+//    var bar: Array<Rhythm> = []
+//    // init a VAR: available values, which is length - runnningTotal
+//    var amountLeftInBar = length
+//    // filter choices to only have vals which are <= availableChoices i.e how much time left
+//
+//    // (if only 0.5 beats left then filter for RHYTHMS whose value is <= 0.5 )
+//    // while total < length
+//    // append a Random rhythm to the array
+//    // array must exact= length
+//
+//
+//    // -> an array of rhythms whos total values = <length>
+//}
